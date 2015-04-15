23ba800882054149af69d8f187486be7d92673e5
ro3t <voss@bfpi.de>
2015-04-15 11:21:47 +0200

    Workaround für besseren Response-Code bei PUT und PATCH (https://github.com/plataformatec/responders/pull/115#issuecomment-72517532)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,9 @@ class ApplicationController < ActionController::Base
   end
 
   rescue_from ErrorMessage do |ex|
-    ex = ErrorMessage.new(ex.message) unless ex.class.to_s.eql?("ErrorMessage")
-    respond_with(Array.wrap(ex), root: :error_messages, dasherize: false, location: discovery_url, status: ex.http_code || 422)
+    ex = ErrorMessage.new(ex.message) unless ex.is_a? ErrorMessage
+    error = Array.wrap(ex)
+    respond_with error, root: :error_messages, dasherize: false, location: discovery_url,
+                 status: ex.http_code || 422, request.format.symbol.to_sym => error
   end
 end

d44b293ce2a1b3684744245a0069a78b40526934
Sebastian Gutzeit <sebastian.gutzeit@rostock.de>
2017-11-23 16:53:36 +0100

    aus Hansestadt Rostock wird Hanse- und Universitätsstadt Rostock

--- app/models/discovery.rb
+++ app/models/discovery.rb
@@ -13,7 +13,7 @@ class Discovery
   end
 
   def contact
-    'Hanse- und Universit‰tsstadt Rostock, Kataster-, Vermessungs- und Liegenschaftsamt, Holbeinplatz 14, 18069 Rostock, Telefon: +49 381 381-6281, Telefax: +49 381 381-6902, E-Mail: klarschiff.hro@rostock.de'
+    'Hanse- und Universit√§tsstadt Rostock, Kataster-, Vermessungs- und Liegenschaftsamt, Holbeinplatz 14, 18069 Rostock, Telefon: +49 381 381-6281, Telefax: +49 381 381-6902, E-Mail: klarschiff.hro@rostock.de'
   end
 
   def key_service
